00af3a6fcde6129f33018339594a256ceceff556
huynhngocnin@rocketmail.com <troidatoi>
2016-04-10 19:30:08 +0700

    Add Facebook login

--- app/src/main/java/ninhn/app/girlxinh/view/FragmentMe.java
+++ app/src/main/java/ninhn/app/girlxinh/view/FragmentMe.java
@@ -1,43 +1,102 @@
 package ninhn.app.girlxinh.view;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 
 import ninhn.app.girlxinh.R;
-import ninhn.app.girlxinh.adapter.PhotoAdapter;
-import ninhn.app.girlxinh.listener.OnItemClickListener;
-import ninhn.app.girlxinh.listener.OnLoadMoreListener;
-import ninhn.app.girlxinh.model.PhotoModel;
 
 /**
  * Created by NinHN on 4/10/16.
  */
 public class FragmentMe extends Fragment {
 
+    private LoginButton loginButton;
+    private TextView me_info;
+    private CallbackManager callbackManager;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //return super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.content_me, container, false);
+        View view = inflater.inflate(R.layout.content_me, container, false);
+
+        FacebookSdk.sdkInitialize(FacebookSdk.getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+
+        me_info = (TextView) view.findViewById(R.id.me_info);
+        loginButton = (LoginButton) view.findViewById(R.id.login_button);
+        //loginButton.setReadPermissions("user_friends"); //"publish_actions"
+        //loginButton.setReadPermissions(Arrays.asList("user_status"));
+        // If using in a fragment
+        loginButton.setFragment(this);
+        // Other app specific specialization
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                AccessToken accessToken = loginResult.getAccessToken();
+                //Toast.makeText(getActivity(), "User ID: " + loginResult.getAccessToken().getUserId() + "\nToken: " + loginResult.getAccessToken().getToken(), Toast.LENGTH_SHORT).show();
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        accessToken,
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(
+                                    JSONObject object,
+                                    GraphResponse response) {
+                                //Toast.makeText(getActivity(), object.toString(), Toast.LENGTH_SHORT).show();
+                                me_info.setText(object.toString());
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,link");
+                request.setParameters(parameters);
+                request.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getActivity(), "Cancen roi nhe ku", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast.makeText(getActivity(), "Error roi! Lam lai di", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
--- app/src/main/java/ninhn/app/girlxinh/view/MainActivity.java
+++ app/src/main/java/ninhn/app/girlxinh/view/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
 import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationView;
 
 import ninhn.app.girlxinh.R;
@@ -20,19 +22,19 @@ public class MainActivity extends AppCompatActivity {
 //    private FloatingActionButton mFabButton;
     private ViewPager viewPager;
 
-    private void hideSystemUI() {
-        View mDecorView = getWindow().getDecorView();
-        // Set the IMMERSIVE flag.
-        // Set the content to appear under the system bars so that the content
-        // doesn't resize when the system bars hide and show.
-        mDecorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
-    }
+//    private void hideSystemUI() {
+//        View mDecorView = getWindow().getDecorView();
+//        // Set the IMMERSIVE flag.
+//        // Set the content to appear under the system bars so that the content
+//        // doesn't resize when the system bars hide and show.
+//        mDecorView.setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
+//    }
 
 
     @Override
@@ -53,12 +55,27 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-
         initViewPager();
 
         initBottomNavigation();
 
         //initRecyclerView();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
     }
 
     private void initViewPager() {
